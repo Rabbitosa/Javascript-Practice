d5be0c4382138c96077bb1d7e9af89bc2ade1a50
github NikolaCzv <nikola.raicic@hotmail.com>
2020-12-06 13:35:43 -0600

    fix

--- rockpaper.js
+++ rockpaper.js
@@ -1,28 +1,29 @@
-function rps(player1, _player2){
-    if (player1 !=="rock"||"paper"||"scissors" || player2 !=="rock"||"paper"||"scissors"){
-        return "Wrong Input!";
-    }else if (player1 == "rock" && _player2 == "rock"){
+function rps(player1, player2){
+    if (player1 === "rock" && player2 === "rock") {
        return "tie!";
-    }else if  (player1 == "rock" && _player2 == "paper"){
+    } else if  (player1 === "rock" && player2 === "paper") {
         return"player 2 wins!";
-    }else if (player1 == "rock" && _player2 == "scissors"){
+    } else if (player1 === "rock" && player2 === "scissors") {
         return "player 1 wins!";
-    }else if (player1 == "paper" && _player2 == "rock"){
+    } else if (player1 === "paper" && player2 === "rock") {
         return "player 1 wins!";
-    }else if (player1 == "paper" && _player2 == "paper"){
+    } else if (player1 === "paper" && player2 === "paper") {
         return "tie!";
-    }else if (player1 == "paper" && _player2 == "scissors"){
+    } else if (player1 === "paper" && player2 === "scissors") {
         return "player 2 wins!";
-    }else if (player1 == "scissors" && _player2 == "rock"){
+    } else if (player1 === "scissors" && player2 === "rock") {
         return "player 2 wins!";
-    }else if (player1 == "scissors" && _player2 == "paper"){
+    } else if (player1 === "scissors" && player2 === "paper") {
         return "player 1 wins!";
-    }else if (player1 == "scissors" && _player2 == "scissor"){
+    } else if (player1 === "scissors" && player2 === "scissors") {
         return "tie!";
+    } else if (player1 !== "rock"|| player1 !== "paper" || player1 !== "scissors" || player2 !== "rock" || player2 !== "paper" || player2 !== "scissors") {
+        return "Wrong Input!";
     };
+
 };
     
 
   
 
-console.log(rps("rock", "scissors"));
+console.log(rps("paper", "paper"));
